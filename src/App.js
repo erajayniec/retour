57fee4727382aa4fd0416c695ff6441a676e6fc2
erajayniec <er.ajayniec@gmail.com>
2022-07-06 23:10:55 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Navbar from './components/inc/navbar';
 import StoreList from './components/pages/storelist';
 import TourLocator from './components/pages/tourlocator';
 import HomePage from './components/pages/homepage';
-import TutorialList from './components/pages/tutorials';
 
 function App() {
   return (
@@ -12,10 +11,11 @@ function App() {
      <div className='container-fluid px-0'>           
        <Navbar />
         <Routes>
-          <Route exact path="/" element={<HomePage />} />
+          <Route exact path="" element={<HomePage />} />   
+          <Route exact path="/" element={<HomePage />} />       
+          <Route exact path="/retourwebsite/" element={<HomePage />} />
           <Route exact path="/storelist" element={<StoreList />} /> 
-          <Route exact path="/tourlocator" element={<TourLocator />} />
-          <Route exact path="/tutorials" element={<TutorialList />} />   
+          <Route exact path="/tourlocator" element={<TourLocator />} />   
         </Routes>
      </div>
      </Router>
