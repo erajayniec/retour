74abc9062e4370f33f67029a74b4b6b1ec6f0773
erajayniec <er.ajayniec@gmail.com>
2022-07-06 23:23:38 +0530

    'react'

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom';
+import { HashRouter, Routes, Route  } from 'react-router-dom';
 import Navbar from './components/inc/navbar';
 import StoreList from './components/pages/storelist';
 import TourLocator from './components/pages/tourlocator';
 import HomePage from './components/pages/homepage';
+import TutorialList from './components/pages/tutorials';
 
 function App() {
   return (
-     <Router>
+     <HashRouter>
      <div className='container-fluid px-0'>           
        <Navbar />
         <Routes>
-          <Route exact path="" element={<HomePage />} />   
-          <Route exact path="/" element={<HomePage />} />       
-          <Route exact path="/retourwebsite/" element={<HomePage />} />
+          <Route exact path="/" element={<HomePage />} />
           <Route exact path="/storelist" element={<StoreList />} /> 
           <Route exact path="/tourlocator" element={<TourLocator />} />   
+          <Route exact path="/tutorials" element={<TutorialList />} /> 
         </Routes>
      </div>
-     </Router>
+     </HashRouter>
   );
 }
 
