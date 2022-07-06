57e5e27aa9a20e468576cd912d5d253e6ffccb47
erajayniec <er.ajayniec@gmail.com>
2022-07-07 00:05:16 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, HashRouter  } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom';
 import Navbar from './components/inc/navbar';
 import StoreList from './components/pages/storelist';
 import TourLocator from './components/pages/tourlocator';
@@ -8,19 +8,17 @@ import TutorialList from './components/pages/tutorials';
 
 function App() {
   return (
-    <BrowserRouter>
-       <HashRouter basename="/">
-       <div className='container-fluid px-0'>           
-         <Navbar />
-          <Routes>
-            <Route exact path="/" element={<HomePage />} />
-            <Route exact path="/storelist" element={<StoreList />} /> 
-            <Route exact path="/tourlocator" element={<TourLocator />} />   
-            <Route exact path="/tutorials" element={<TutorialList />} /> 
-          </Routes>
-       </div>
-       </HashRouter>
-      </BrowserRouter>
+     <Router>
+     <div className='container-fluid px-0'>           
+       <Navbar />
+        <Routes>
+          <Route exact path="/" element={<HomePage />} />
+          <Route exact path="/storelist" element={<StoreList />} /> 
+          <Route exact path="/tourlocator" element={<TourLocator />} />   
+          <Route exact path="/tutorials" element={<TutorialList />} /> 
+        </Routes>
+     </div>
+     </Router>
   );
 }
 
